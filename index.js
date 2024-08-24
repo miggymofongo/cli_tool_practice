fdfc5af7edcdc3955413ccc9d5ba5d43c5d607d0
Miguel <contact@miguelalmodo.com>
2024-08-24 00:15:51 -0400

    messin with some cli libraries

---
+++ index.js
@@ -0,0 +1,82 @@
+#!/usr/bin/env node
+
+import chalk from 'chalk';
+import inquirer from 'inquirer';
+import gradient from 'gradient-string';
+import chalkAnimation from 'chalk-animation';
+import figlet from 'figlet';
+import { createSpinner } from 'nanospinner';
+
+
+let playerName;
+
+const sleep = (ms = 2000) => new Promise((r) => setTimeout(r, ms));
+
+async function welcome() {
+    const rainbowTitle = chalkAnimation.rainbow(
+        'Oye, que hay? \n ');
+await sleep();
+
+rainbowTitle.stop();
+
+console.log(`
+    ${chalk.bgBlue('manual de instruciones')}
+    Soy un proceso en tu computadora.
+    Si se equivoca en una pregunta, este programa ${chalk.bgRed('terminará')}. 
+    Así que acierta todas las preguntas...
+    
+    `);
+
+    }
+
+
+
+async function preguntaPorNombre() {
+    const answers = await inquirer.prompt({
+        name: 'player_name',
+        type: 'input',
+        message: 'What is your name?',
+        default() {
+            return 'Player';
+        },
+    });
+
+    playerName = answers.player_name;
+}
+
+
+
+// await preguntaPorNombre();
+
+async function pregunta1() {
+    const answer = await inquirer.prompt({
+        name: 'pregunta_1',
+        type: 'list',
+        message: 'Nohelymar nació en el año\n',
+        choices: [
+            '2000',
+            '1987',
+            '1993',
+            '1999',
+        ],
+    });
+return handleAnswer(answer.pregunta_1 == 2000)
+}
+
+
+
+async function handleAnswer(isCorrect) {
+    const spinner = createSpinner('Chequiando la respuesta...').start();
+    await sleep();
+
+    if (isCorrect) {
+        spinner.success({ text: `Wepa, ${playerName}! Has respondido correctamente`});
+    } else {
+        spinner.error({ text: `perrrroooo perdiste ${playerName}`});
+        process.exit(1);
+    }
+}
+
+await welcome();
+await preguntaPorNombre();
+await pregunta1();
